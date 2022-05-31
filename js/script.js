933fada4e86ea66615d2e388480b9af743b404bd
Vinicius de Freitas <91574553+ViniciusF-Pereira@users.noreply.github.com>
2022-05-31 10:59:41 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -23,15 +23,30 @@ function jogar() {
 
 
 
-    var nrCorComida = 0; // para trocar a cor da comida ( piscando)
-    var corComida = []; //to change food color (flashing)
+    var FrutasNumero = 0; // para trocar a cor da comida ( piscando)
+    var Frutas = []; //to change food color (flashing)
 
 
-    corComida[0] = "#FF6347";
-    corComida[1] = "#FF0000";
-    corComida[2] = "#FF4500";
+    Frutas[0] = "/assets/comidas/banan.jpg";
+    Frutas[1] = "/assets/comidas/maca.png";
+    Frutas[2] = "/assets/comidas/cherry.jpg";
+    Frutas[3] = "/assets/comidas/lemon.jpg";
+    Frutas[4] = "/assets/comidas/strawberry.jpg";
 
+    var snakeHeaddirectionNumero = 0;
+    var snakeHeaddirection = [];
 
+    snakeHeaddirection[0] = "/assets/Snake/snakeHead_direita.png";
+    snakeHeaddirection[1] = "/assets/Snake/snakeHead_esquerda.png";
+    snakeHeaddirection[2] = "/assets/Snake/snakeHead_cima.png";
+    snakeHeaddirection[3] = "/assets/Snake/snakeHead_baixo.png";
+
+    var snakeTailImageDirection = [];
+
+    snakeTailImageDirection[0] = "/assets/Snake/snakeTail_direita.png";
+    snakeTailImageDirection[1] = "/assets/Snake/snakeTail_esquerda.png";
+    snakeTailImageDirection[2] = "/assets/Snake/snakeTail_cima.png";
+    snakeTailImageDirection[3] = "/assets/Snake/snakeTail_baixo.png";
 
     let vida = 70; // vida da cobrinha
     let vidamenos = 100;
@@ -69,22 +84,41 @@ function jogar() {
 
 
     //-------------------------------------------------------------- FUNÇÕES DESENHO / DRAWING
-
-    //----------------------------------------------  Função Criar Campo / Function CREATE Fild
-    function criarBG() {
-        context.fillStyle = "#00FF7F";
-        context.fillRect(0, 0, 16 * box, 16 * box);
-        //  Define o campo usando em x e y e a largura e altura setadas
-        //      Define the fild of the game
+    if (playerCanPlay == true) {
+        //----------------------------------------------  Função Criar Campo / Function CREATE Fild
+        function criarBG() {
+            context.fillStyle = "#00FF7F";
+            context.fillRect(0, 0, 16 * box, 16 * box);
+            //  Define o campo usando em x e y e a largura e altura setadas
+            //      Define the fild of the game
+        }
     }
     //---------------------------------------------- Função Criar Cobra / Fuction CREATE Snake
     function criarCobrinha() {
 
 
         for (i = 0; i < snake.length; i++) {
-            context.fillStyle = "#556B2F";
 
-            context.fillRect(snake[i].x, snake[i].y, box, box);
+
+            // aqui defino o corpo dela
+
+            snakeBodyImage = new Image();
+
+            snakeBodyImage.src = "/assets/Snake/snakeBody.png";
+
+            context.drawImage(snakeBodyImage, snake[i].x, snake[i].y, box, box);
+
+            // vou por o rabo dela agora.
+
+
+            // aqui eu coloco a cabeça da cobra
+            snakeHeadImage = new Image();
+
+            snakeHeadImage.src = snakeHeaddirection[snakeHeaddirectionNumero];
+
+            context.drawImage(snakeHeadImage, snake[0].x, snake[0].y, box, box);
+
+
 
             // Define o tamanho da cobra por uma função
 
@@ -106,19 +140,53 @@ function jogar() {
     function drawFood() {
 
         //para trocar a cor da comida ( piscando) em um loop
-        comidinha = corComida[nrCorComida++]; //to change food color (flashing) in a loop
-
-        if (nrCorComida >= corComida.length) {
-            nrCorComida = 0;
-
-        }
+        //to change food color (flashing) in a loop
 
 
-        context.fillStyle = comidinha;
-        context.fillRect(food.x, food.y, box, box);
+        comidinha = new Image();
+        comidinha.src = Frutas[FrutasNumero];
+
+        context.drawImage(comidinha, food.x, food.y, box, box);
+
 
 
     }
+
+
+
+
+    let inseto = {
+
+
+        x: Math.floor(Math.random() * 15 + 1) * box,
+        // Math.floor -> removes the floating point from Math.Random (the comma), as far as they were set in CANVAS
+        y: Math.floor(Math.random() * 15 + 1) * box
+
+
+
+    }
+
+
+
+    function drawinseto() {
+
+
+
+        base_image = new Image();
+        base_image.src = "joaninha.jpg";
+
+
+
+
+        context.drawImage(base_image, inseto.x, inseto.y, box, box);
+
+
+
+
+    }
+
+
+
 
 
 
@@ -147,6 +215,8 @@ function jogar() {
         // if the snake hits the wall, it gets a new property and reappears in an infinite loop
 
         if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+
+
         if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
 
         if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
@@ -167,7 +237,7 @@ function jogar() {
                 audio2.play();
 
                 playerCanPlay = true;
-                alert('Game Over :(');
+
 
                 document.getElementById("pontuacao").innerHTML = pontos;
                 ranking[lista] = pontos;
@@ -184,7 +254,30 @@ function jogar() {
                 pontos = 0;
                 lista++
             }
+            if (snake[0].x == inseto.x && snake[0].y == inseto.y) {
+                clearInterval(jogo);
 
+                var audio2 = document.getElementById('audio2');
+                audio2.play();
+
+                playerCanPlay = true;
+
+
+                document.getElementById("pontuacao").innerHTML = pontos;
+                ranking[lista] = pontos;
+
+
+
+
+                document.getElementById("pont0").innerHTML = ranking[0];
+                document.getElementById("pont1").innerHTML = ranking[1];
+                document.getElementById("pont2").innerHTML = ranking[2];
+                document.getElementById("pont3").innerHTML = ranking[3];
+                document.getElementById("pont4").innerHTML = ranking[4];
+                vida = 100;
+                pontos = 0;
+                lista++
+            }
 
 
             vidamenos = 100 - vida;
@@ -208,7 +301,7 @@ function jogar() {
                 playerCanPlay = true;
                 clearInterval(jogo);
 
-                alert('Game Over :(');
+
                 vida = 100;
                 pontos = 0;
 
@@ -230,7 +323,7 @@ function jogar() {
         criarBG();
         criarCobrinha();
         drawFood();
-
+        drawinseto();
 
         let snakeX = snake[0].x; // 0 -> X 
         let snakeY = snake[0].y; // 0 -> Y
@@ -240,11 +333,26 @@ function jogar() {
         // Cria condicional. para o lado que ela ir, diminuir 1 quadrado ao lado contrario da direção da cobra.
         // Create conditional. which way it goes, decrease 1 square to the opposite side of the snake's direction.
 
-        if (direction == "right") snakeX += box; //     DIREITA
-        if (direction == "left") snakeX -= box; //      ESQUERRDA
+        if (direction == "right") {
+            snakeX += box;
+            snakeHeaddirectionNumero = 0;
+        } //     DIREITA
+
+
+        if (direction == "left") {
+            snakeX -= box;
+            snakeHeaddirectionNumero = 1
+        }
+        //      ESQUERRDA
         //------------
-        if (direction == "up") snakeY -= box; //     CIMA
-        if (direction == "down") snakeY += box; //   BAIXO
+        if (direction == "up") {
+            snakeY -= box;
+            snakeHeaddirectionNumero = 2;
+        } //     CIMA
+        if (direction == "down") {
+            snakeY += box;
+            snakeHeaddirectionNumero = 3;
+        } //   BAIXO
 
 
         //|↑                                        |↑
@@ -269,9 +377,28 @@ function jogar() {
 
 
 
-            food.x = Math.floor(Math.random() * 15 + 1) * box;
+            console.log(Frutas[FrutasNumero]);
+
+
+            food_x = Math.floor(Math.random() * 15 + 1) * box;
+            food_y = Math.floor(Math.random() * 15 + 1) * box;
+            if (food_x != inseto.x && food_x != snake.x) {
+
+                food.x = food_x;
+            } else {
+                food_x = Math.floor(Math.random() * 15 + 1) * box;
+            }
+
+            if (food_y != inseto.y && food_y != snake.y) {
+
+                food.y = food_y;
+            } else {
+                food_y = Math.floor(Math.random() * 15 + 1) * box;
+            }
+
+
+
             // ao passar a cobra na comida, ela gera a comida em outro lugar
-            food.y = Math.floor(Math.random() * 15 + 1) * box;
             // when passing the snake in the food, it generates the food elsewhere
 
             var audio4 = document.getElementById('audio4');
@@ -280,14 +407,19 @@ function jogar() {
             if (vida > 100) {
                 vida = 100;
             }
+
+
+            FrutasNumero = Math.floor(Math.random() * 4);
+
             pontos++;
             document.getElementById("pontuacao").innerHTML = pontos;
 
-
-
-
-
         }
+
+
+
+
+
 
 
         //---------------------------------------------- Função para cabeça da Cobra / Function for Snake Head
@@ -297,13 +429,46 @@ function jogar() {
             x: snakeX,
             y: snakeY
 
+
+
         }
+
+
+
+
 
         snake.unshift(newHead); // Adiciona 1 no valor da cabeça / Add 1 to the head value
 
 
 
         vida--;
+
+        if (_tempo == inseto_tempo) {
+
+            inseto_x = Math.floor(Math.random() * 15 + 1) * box;
+            inseto_y = Math.floor(Math.random() * 15 + 1) * box;
+            if (inseto_x != food.x && inseto_x != snake.x) {
+
+                inseto.x = inseto_x;
+            } else {
+                inseto_x = Math.floor(Math.random() * 15 + 1) * box;
+            }
+
+            if (inseto_y != food.y && inseto_y != snake.y) {
+
+                inseto.y = inseto_x;
+            } else {
+                inseto_y = Math.floor(Math.random() * 15 + 1) * box;
+            }
+
+
+            _tempo = 0;
+
+        }
+
+
+        _tempo++;
+        console.log(_tempo);
 
 
     }
@@ -314,7 +479,20 @@ function jogar() {
     // Define um intervalo de tempo para iniciar o jogo e renovar o jogo sem ele travar
     // Sets a time interval to start the game and renew the game without it crashing
 
+
+    var _tempo = 1;
+    var inseto_tempo = 100;
+
+
+
 }
+
+
+
+
+var audioFundo = document.getElementById('audioFundo');
+
+audioFundo.autoplay = true;
 
 // INICIAR O JOGO / START THE GAME
 var btn = document.querySelector("#refresh");
@@ -326,7 +504,11 @@ btn.addEventListener("click", function() {
         var audio3 = document.getElementById('audio3');
         audio3.play();
         document.getElementById("pontuacao").innerHTML = pontos;
+
         jogar();
+
+
+
         playerCanPlay = false;
 
     }
